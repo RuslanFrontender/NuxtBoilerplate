1dc0aeee8924ffcee29280ba7886d36d61c7e6bb
Ruslan Rybalka <ruslanrybalka@thefirstthelast.agency>
2021-11-28 17:19:09 +0200

    Add some scss styles

--- stylelint.config.js
+++ stylelint.config.js
@@ -11,5 +11,8 @@ module.exports = {
   rules: {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
+    "scss/no-duplicate-mixins": true,
+    "scss/at-import-no-partial-leading-underscore": true,
+    "scss/dimension-no-non-numeric-values": true,
   }
 }
